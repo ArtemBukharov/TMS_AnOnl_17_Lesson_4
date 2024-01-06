e0c164ff01b90437267309d12dccbad857836bbd
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-01-06 23:23:18 +0300

    Task3

--- src/Task3.java
+++ src/Task3.java
@@ -9,15 +9,39 @@ public class Task3 {
         int size = scanner.nextInt();
         int[] array = new int[size];
         for (int i = 0; i < array.length; i++) {
-            array[i]= (int)(Math.random()*100) ;
-
+            array[i] = (int) (Math.random() * 10);
 
 
         }
         System.out.println(Arrays.toString(array));
+
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        double average = (double) sum / array.length;
+
+        System.out.println( max );
+        System.out.println( min );
+        System.out.println( average );
+
+
     }
-
-
 }
+
 
 
