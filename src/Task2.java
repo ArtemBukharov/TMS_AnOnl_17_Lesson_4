f300e281abf35f5d1fc8676184f6ec157cf734c7
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-01-07 16:45:57 +0300

    Task2

--- src/Task2.java
+++ src/Task2.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number");
+        int[] array = {1,3,5,7,9,33,14,3};
+        int counter = 0;
+        System.out.println("Введите число из массива ");
         int number = scanner.nextInt();
-        int[] array = new int[]{1, 5, 6, 7, 9, 11};
-        int[] resultarray = new int[array.length - 1];
-        System.out.println("do" + Arrays.toString(array));
-        for (int i = 0, k = 0; i < array.length; i++) {
-            if (array[i] != number) {
-                resultarray[k]=array[i];
-                k++;
+        for (int i : array) {
+            if (number == i) {
+                counter++;
             }
-
-
-
         }
-        System.out.println("posle" + Arrays.toString(resultarray));
+        if (counter != 0) {
+            int[] resultarray = new int[array.length - counter];
+            for (int i = 0, j = 0; i < array.length; i++) {
+                if (number != array[i]) {
+                    resultarray[j] = array[i];
+                    j++;
+                }
+            }
+            System.out.println("do"+ Arrays.toString(array));
+            System.out.println("posle"+ Arrays.toString(resultarray));
+        } else {
+            System.out.println("Tакого числа нет в массиве");
+        }
     }
-
-
 }
